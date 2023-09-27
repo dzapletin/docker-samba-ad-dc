17b810c748b20193db756d66fd3b036bd01592d3
Dmitry Zapletin <129387793+dzapletin@users.noreply.github.com>
2023-09-27 11:56:44 +0300

    update

--- samba-ad-dc/sync-sysvol.sh
+++ samba-ad-dc/sync-sysvol.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-chwown 400 /run/secrets/rsync_sysvol_password
-/usr/bin/rsync $@ -XAavz --delete-after --password-file=/run/secrets/rsync_sysvol_password rsync://$(cat /run/secrets/rsync_sysvol_user)@${PDC_IP}/SysVol/ /var/lib/samba/sysvol/
+/usr/bin/cp /run/secrets/rsync_sysvol_password /etc/samba/rsyncd.secret
+/usr/bin/chmod 400 /etc/samba/rsyncd.secret
+/usr/bin/rsync $@ -XAavz --delete-after --password-file=/etc/samba/rsyncd.secret rsync://$(cat /run/secrets/rsync_sysvol_user)@${PDC_IP}/SysVol/ /var/lib/samba/sysvol/
